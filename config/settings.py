a5d412bd91aced4c22d9071fcf7d455173304152
Alli Olarinde <olarindeallitaiwo@gmail.com>
2021-04-27 21:40:59 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0b9s%k-7emcswx9=5i-yhiar2wc39+bi#-5t*qv(#r_gf*xp7-"
-
+# SECRET_KEY = "django-insecure-0b9s%k-7emcswx9=5i-yhiar2wc39+bi#-5t*qv(#r_gf*xp7-"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["polymathdjangoapp.herokuapp.com"]
 
 
 # Application definition
